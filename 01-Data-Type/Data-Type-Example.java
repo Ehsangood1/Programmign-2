afae717284c96ed9a072255787a4c85b8b8ef75d
Ehsan Goodarzi <124208792+Ehsangood1@users.noreply.github.com>
2024-07-02 11:13:53 +0200

    Create Data-Type-Example.java

---
+++ 01-Data-Type/Data-Type-Example.java
@@ -0,0 +1,10 @@
+   public static void main(String[] args) {
+        int age = 30;
+        double price = 19.99;
+        char grade = 'A';
+        boolean isAvailable = true;
+
+        System.out.println(age+" " + price+" " + grade+" " + grade+" " + isAvailable);
+
+
+    }
