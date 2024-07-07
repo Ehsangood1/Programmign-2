a58adaa5c03fec2294b44ee82107d83d014100f8
Ehsan Goodarzi <124208792+Ehsangood1@users.noreply.github.com>
2024-07-07 08:15:02 +0200

    Update Data-Type-Example2.java

--- 01-Data-Type/Data-Type-Example2.java
+++ 01-Data-Type/Data-Type-Example2.java
@@ -1,5 +1,27 @@
-public static void main(String[] args) {
-String name = "John";
-int[] numbers = {1, 2, 3, 4, 5};
-ArrayList<String> names = new ArrayList<>();
+
+
+// Reference data types
+        String str = "Hello, World!";
+        int[] arr = {1, 2, 3, 4, 5};
+
+        // Output reference data types
+        System.out.println("String: " + str);
+        System.out.print("Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Using a class
+        Person p = new Person();
+        p.name = "John";
+        p.age = 30;
+        System.out.println("Person name: " + p.name);
+        System.out.println("Person age: " + p.age);
+    }
+}
+
+class Person {
+    String name;
+    int age;
 }
