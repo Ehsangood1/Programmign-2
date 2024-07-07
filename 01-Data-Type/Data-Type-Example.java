2b214c3ceec66bef4783ae1a73c5a7e9d9aeefdb
Ehsan Goodarzi <124208792+Ehsangood1@users.noreply.github.com>
2024-07-07 08:08:00 +0200

    Update Data-Type-Example.java

--- 01-Data-Type/Data-Type-Example.java
+++ 01-Data-Type/Data-Type-Example.java
@@ -1,10 +1,23 @@
-   public static void main(String[] args) {
-        int age = 30;
-        double price = 19.99;
-        char grade = 'A';
-        boolean isAvailable = true;
+public class DataTypesExample {
+    public static void main(String[] args) {
+        // Primitive data types
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000000L;
+        float f = 10.5f;
+        double d = 20.99;
+        char c = 'A';
+        boolean bool = true;
 
-        System.out.println(age+" " + price+" " + grade+" " + grade+" " + isAvailable);
-
+        // Output primitive data types
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
 
     }
