5ec0e7cc85181f0efe5fdda8ef1a64d958a531d8
Ehsan Goodarzi <124208792+Ehsangood1@users.noreply.github.com>
2024-07-07 10:03:07 +0200

    Create Example3.java

---
+++ 02-Input-Output/Example3.java
@@ -0,0 +1,12 @@
+public class ConsoleOutputExample {
+    public static void main(String[] args) {
+        // Printing a message
+        System.out.print("Hello, ");
+        System.out.println("World!");
+
+        // Printing formatted text
+        int year = 2024;
+        double pi = 3.14159;
+        System.out.printf("The year is %d and the value of Pi is approximately %.2f%n", year, pi); // %d , this is a placeholder for an integer, %.2f%n this is placeholder for floating-point.
+    }
+}
