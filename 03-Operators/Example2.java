428144922317fa52a5877d2c67f8240071cb8078
Ehsan Goodarzi <124208792+Ehsangood1@users.noreply.github.com>
2024-07-08 17:40:57 +0200

    Create Example2.java

---
+++ 03-Operators/Example2.java
@@ -0,0 +1,9 @@
+public class RelationalExample {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+        System.out.println("a > b: " + (a > b));   // false
+        System.out.println("a < b: " + (a < b));   // true
+    }
+}
