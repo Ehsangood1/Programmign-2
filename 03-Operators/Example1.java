c468e15455754987d5b60eb5949243fd45b2a798
Ehsan Goodarzi <124208792+Ehsangood1@users.noreply.github.com>
2024-07-08 17:35:51 +0200

    Update Example1.java

--- 03-Operators/Example1.java
+++ 03-Operators/Example1.java
@@ -1,1 +1,10 @@
-
+public class ArithmeticExample {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+        System.out.println("a + b = " + (a + b)); // 13
+        System.out.println("a - b = " + (a - b)); // 7
+        System.out.println("a * b = " + (a * b)); // 30
+        System.out.println("a / b = " + (a / b)); // 3
+        System.out.println("a % b = " + (a % b)); // 1
+    }
+}
