c0431d5af3ad4e2e93f1199edb5393791e9c5daa
Ehsan Goodarzi <124208792+Ehsangood1@users.noreply.github.com>
2024-07-08 18:25:53 +0200

    Update Example3.java

--- 03-Operators/Example3.java
+++ 03-Operators/Example3.java
@@ -1,1 +1,8 @@
-
+public class LogicalExample {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println("a && b: " + (a && b)); // false
+        System.out.println("a || b: " + (a || b)); // true
+        System.out.println("!a: " + !a);           // false
+    }
+}
